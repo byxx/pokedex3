34cfaed07d96bcb3bbf2298fb2cc505d6fea1d4d
your name <your@email.com>
2017-01-16 07:01:26 +0100

    unfinished Vid3

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,13 @@
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Tim on 16.01.17.
+//  Copyright © 2017 byxx. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
